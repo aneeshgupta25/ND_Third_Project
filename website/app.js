caba7068d97ff508a89241298aec107ab9e07bbe
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-06-05 13:48:27 +0530

    Rectified Basic Functionality

--- website/app.js
+++ website/app.js
@@ -1,9 +1,9 @@
 //client-side
 // Personal API Key for OpenWeatherMap API
-const apiKey = '';
+const apiKey = 'aed7fea30e09c377e19166172ff010d0&units=imperial';
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?'
 
-const postData = async (data={})=> {    
+const postData = async (data={}) => {    
     const url = '/postWeather';
     const response = await fetch(url, {
         method: 'POST', 
@@ -14,9 +14,12 @@ const postData = async (data={})=> {
         body: JSON.stringify(data)
     });
 
-    try {
-        const newData = await response.json();
-        console.log("Testing Data" + newData);
+    try {        
+        // console.log(response);
+        // console.log(response.status);
+        const newData = await response.json();        
+        // console.log(newData);
+        console.log(newData)
         return newData;
     } catch(e) {
         console.log("ERROR OCCURRED -> My Server - POST");   
@@ -27,7 +30,8 @@ const getDataFromOpenWeather = async (data={}) => {
     const response = await fetch(`${baseURL}q=${data.value}&appid=${apiKey}`);    
     try {
         const newData = await response.json();
-        console.log(`New Data From OpenWeatherAPI -> ${newData}`);        
+        console.log(`New Data From OpenWeatherAPI -> ${newData}`);    
+        console.log(newData)
         return newData;
     } catch(e) {
         console.log("ERROR OCCURRED -> OpenWeatherAPI");
@@ -35,7 +39,8 @@ const getDataFromOpenWeather = async (data={}) => {
 }
 
 const getData = async (data) => {
-    const response = await fetch(`/weatherInfo?key=${data.key}`);
+    // console.log(data.value);
+    const response = await fetch(`/weatherInfo?q=${data.value}`);
     try {
         const newData = await response.json();
         console.log(`New Data From My Server -> ${newData}`);
@@ -45,5 +50,33 @@ const getData = async (data) => {
     }
 }
 
-const myData = getDataFromOpenWeather({ value: '9000' });
-const myData2 = postData({key: '9000', value: myData});
+document.getElementById('generate').addEventListener('click', getDataAndUpdateUI);
+function getDataAndUpdateUI() {
+    const fav = document.getElementById("feelings").value;
+    const areaCode = document.getElementById("zip").value;
+
+    getDataFromOpenWeather({value: areaCode})
+    .then(        
+        function(data) {
+            postData({
+                key: areaCode,
+                value: {
+                    main: data.weather[0].main,
+                    description: data.weather[0].description,
+                    temp: data.main.temp,
+                    name: data.name,
+                    fav: fav                    
+                }
+            })           
+        },        
+    )
+    .then(() => getData({value: areaCode}))      
+    .then((data) => {
+        document.getElementById('temp').innerHTML = `${Math.round(data.temp)} °C`;        
+    });
+
+}
+
+const updateUI = async () => {
+    const response = await fetch('')
+} 
