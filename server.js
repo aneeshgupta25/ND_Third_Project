0811281541994877e213ec19f2d6b336c4d2daba
aneeshgupta25 <aneeshgupta2512@gmail.com>
2023-07-06 11:05:32 +0530

    reset var

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const cors = require('cors');
 const projectData = {};
 const app = express();
 
+function resetVariables() {
+    projectData = {};
+}
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(express.static('website'));
 app.use(express.static('website'));
+
+resetVariables();
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, listening);
